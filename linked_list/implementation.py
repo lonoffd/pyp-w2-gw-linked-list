0f7e63f7ad5e0e9589db23023f89b0eadaa9509b
David Lonoff <david.lonoff@gmail.com>
2017-05-06 15:56:20 -0400

    Tests passing!

--- linked_list/implementation.py
+++ linked_list/implementation.py
@@ -1,5 +1,5 @@
 from .interface import AbstractLinkedList
-
+from .node import Node
 
 class LinkedList(AbstractLinkedList):
     """
@@ -7,34 +7,96 @@ class LinkedList(AbstractLinkedList):
     """
 
     def __init__(self, elements=None):
-        pass
+        self.start = None
+        self.end = None
+        self._count = 0
+        if elements:
+            for element in elements:
+                self.append(element)
 
     def __str__(self):
-        pass
+        inner_string = ", ".join(str(element) for element in self)
+        return "[{}]".format(inner_string)
 
     def __len__(self):
-        pass
+        return self._count
 
     def __iter__(self):
-        pass
+        current_node = self.start
+        while current_node is not None:
+            yield current_node.elem
+            current_node = current_node.next
+        raise StopIteration
 
     def __getitem__(self, index):
-        pass
+        if index >= self._count:
+            raise IndexError
+        it = iter(self)
+        for i in range(index):
+            next(it)
+        return next(it)
 
     def __add__(self, other):
-        pass
+        new_ll = LinkedList()
+        for elem in self:
+            new_ll.append(elem)
+        for elem in other:
+            new_ll.append(elem)
+        return new_ll
 
     def __iadd__(self, other):
-        pass
+        for elem in other:
+            self.append(elem)
+        return self
 
     def __eq__(self, other):
-        pass
+        if len(self) != len(other):
+            return False
+        return all(n1 == n2 for n1, n2 in zip(self, other))
+
+    def __ne__(self, other):
+        return not self == other
 
     def append(self, elem):
-        pass
+        new_node = Node(elem)
+        if self.start is None:
+            self.start = new_node
+            self.end = new_node
+        else:
+            self.end.next = new_node
+            self.end = self.end.next
+
+        self._count += 1
 
     def count(self):
-        pass
+        return self._count
 
     def pop(self, index=None):
-        pass
+        if index is None:
+            index = self._count - 1
+
+        if index >= self._count or self._count == 0:
+            raise IndexError
+
+        if index == 0:
+            # pop start item in list, so just need to get that item
+            # and then set start to be the next item
+            elem = self.start.elem
+            self.start = self.start.next
+        else:
+            previous_node = self.start
+            current_node = self.start.next
+
+            for i in range(index - 1):
+                previous_node = current_node
+                current_node = current_node.next
+
+            elem = current_node.elem
+            previous_node.next = current_node.next
+
+            if index == self._count - 1:
+                # deal with popping the final element
+                self.end = previous_node
+
+        self._count -= 1
+        return elem
--- linked_list/node.py
+++ linked_list/node.py
@@ -4,13 +4,19 @@ class Node(object):
     """
 
     def __init__(self, elem, next=None):
-        pass
+        self.elem = elem
+        self.next = next
 
     def __str__(self):
-        pass
+        if self.next is None:
+            return "Node({}) > /".format(self.elem)
+        else:
+            return "Node({}) > Node({})".format(self.elem, self.next.elem)
 
     def __eq__(self, other):
-        pass
+        return self.elem == other.elem
 
     def __repr__(self):
-        pass
+        if self.next is None:
+            return "Node({})".format(self.elem)
+        return "Node({}, {})".format(self.elem, self.next.elem)
